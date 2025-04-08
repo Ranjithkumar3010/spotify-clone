db9496698b3f51a975283b582795df5d41848c95
Ranjithkumar3010 <ranjith6563@gmail.com>
2025-04-08 15:36:33 +0530

    changes

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,66 +1,64 @@
-const base = process.env.PUBLIC_URL;
-
 
 const songs = [
   {
     title: "Believer",
     artistName: "Imagine Dragons",
-    thumbnail: `${base}/assets/images/Believer.jpg`,
-    musicUrl: `${base}/assets/songs/Believer.mp3`,
+    thumbnail: "/public/assets/images/Believer.jpg",
+    musicUrl: "/public/assets/songs/Believer.mp3",
   },
   {
     title: "Enemy",
     artistName: "Imagine Dragons",
-    thumbnail: `${base}/assets/images/Enemy.jpg`,
-    musicUrl: `${base}/assets/songs/Enemy.mp3`,
+    thumbnail: "/assets/images/Enemy.jpg",
+    musicUrl: "/assets/songs/Enemy.mp3",
   },
   {
     title: "Bones",
     artistName: "Imagine Dragons",
-    thumbnail: `${base}/assets/images/Bones.jpg`,
-    musicUrl: `${base}/assets/songs/Bones.mp3`,
+    thumbnail: "/assets/images/Bones.jpg",
+    musicUrl: "/assets/songs/Bones.mp3",
   },
   {
     title: "Flowers",
     artistName: "Miley Cyrus",
-    thumbnail: `${base}/assets/images/Flowers.jpg`,
-    musicUrl: `${base}/assets/songs/Flowers.mp3`,
+    thumbnail: "/assets/images/Flowers.jpg",
+    musicUrl: "/assets/songs/Flowers.mp3",
   },
   {
     title: "Anti-Hero",
     artistName: "Taylor Swift",
-    thumbnail: `${base}/assets/images/AntiHero.jpg `,
-    musicUrl: `${base}/assets/songs/AntiHero.mp3`,
+    thumbnail: "/assets/images/AntiHero.jpg",
+    musicUrl: "/assets/songs/AntiHero.mp3",
   },
   {
     title: "As It Was",
     artistName: "Harry Styles",
-    thumbnail: `${base}/assets/images/AsItWas.jpg`,
-    musicUrl: `${base}/assets/songs/AsItWas.mp3`,
+    thumbnail: "/assets/images/AsItWas.jpg",
+    musicUrl: "/assets/songs/AsItWas.mp3",
   },
   {
     title: "Unholy",
     artistName: "Sam Smith & Kim Petras",
-    thumbnail: `${base}/assets/images/Unholy.jpeg`,
-    musicUrl: `${base}/assets/songs/Unholy.mp3`,
+    thumbnail: "/assets/images/Unholy.jpeg",
+    musicUrl: "/assets/songs/Unholy.mp3",
   },
   {
     title: "Calm Down",
     artistName: "Rema & Selena Gomez",
-    thumbnail: `${base}/assets/images/CalmDown.jpg`,
-    musicUrl: `${base}/assets/songs/CalmDown.mp3`,
+    thumbnail: "/assets/images/CalmDown.jpg",
+    musicUrl: "/assets/songs/CalmDown.mp3",
   },
   {
     title: "Blinding Lights",
     artistName: "The Weeknd",
-    thumbnail: `${base}/assets/images/BlindingLights.png`,
-    musicUrl: `${base}/assets/songs/BlindingLights.mp3`,
+    thumbnail: "/assets/images/BlindingLights.png",
+    musicUrl: "/assets/songs/BlindingLights.mp3",
   },
   {
     title: "Creepin’",
     artistName: "Metro Boomin ft. The Weekndr",
-    thumbnail: `${base}/assets/images/Creepin.jpeg`,
-    musicUrl: `${base}/assets/songs/Creepin.mp3`,
+    thumbnail: "/assets/images/Creepin.jpeg",
+    musicUrl: "/assets/songs/Creepin.mp3",
   }
 ];
 
